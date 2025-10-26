173b7a5c7c2c5669258d5618136b91aa83b45358
engineer1993 <janjan172020@gmail.com>
2025-10-26 23:21:18 +0800

    Change admin password to plaintext for clarity
    Updated admin password in settings.js for clarity.

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ module.exports = {
         type: "credentials",
         users: [{
             username: "admin",
-            password: "$2a$08$zZJ6u9Kj5bLXq7YkUfNQ5eVHvM9pZnSdIcFwP0BhC2tEiOgJ6Km1K", // Password: admin123
+            password: "admin123", // Password: admin123
             permissions: "*"
         }]
     },
